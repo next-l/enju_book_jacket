17dfceb94ad2bbf7e1a079c72008b7a5bab0bd34
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-06-22 11:16:18 +0900

    updated helper

--- lib/enju_book_jacket/book_jacket_helper.rb
+++ lib/enju_book_jacket/book_jacket_helper.rb
@@ -58,5 +58,27 @@ module EnjuBookJacket
       return nil if asin.blank?
       "http://#{hostname}/dp/#{asin}"
     end
+
+    def book_jacket_source_link
+      case configatron.book_jacket.source
+      when :google
+        link_to "Google Books", "http://books.google.com/"
+      when :amazon
+        link_to "Amazon Web Services", "http://aws.amazon.com/"
+      end
+    end
+
+    def screenshot_generator_link
+      case configatron.screenshot.generator
+      when :mozshot
+        link_to "MozShot", "http://mozshot.nemui.org/"
+      when :simpleapi
+        link_to "SimpleAPI", "http://img.simpleapi.net/"
+      when :heartrails
+        link_to "HeartRails Capture", "http://capture.heartrails.com/"
+      when :thumbalizr
+        link_to "thumbalizr", "http://www.thumbalizr.com/"
+      end
+    end
   end
 end
